f9044bd3dcf7fef2d09a40456c11ac44bf31ed90
유경석 <yks6612@gmail.com>
2022-02-05 17:54:08 +0900

    ~p111

--- App.js
+++ App.js
@@ -1,22 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import {StatusBar} from 'expo-status-bar';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import Mycomponent from "./components/Mycomponent";
+import Heading from "./components/Heading";
+import Input from "./components/Input";
 
-export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Mycomponent/>
-        <StatusBar style="auto" />
-      </View>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            todos: [],
+            type: 'All'
+        }
+    }
+
+    inputChange(inputValue){
+        console.log(' Input Value: ', inputValue);
+        this.setState({inputValue});
+    }
+
+    render() {
+
+        const { inputValue } = this.state
+
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Mycomponent/>
+                <StatusBar style="auto"/>
+                <ScrollView keyboardShouldPersistTaps='always'
+                            style={styles.content}>
+                    <Heading/>
+                    <Input
+                        inputValue={inputValue}
+                        inputChange={(text) => this.inputChange(text)}/>
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    }
 });
+
+export default App;
